52451f6e2a343d4a640ec77b5f65f6c3735bef13
Ashley Noriega <aenorieg@trgn.usc.edu>
2019-09-17 20:18:33 -0700

    Add switch logic

--- `query_server.sh
+++ `query_server.sh
@@ -1,2 +1,20 @@
 #!/usr/bin/bash
-echo "Current server:$HOSTNAME!"
+if [ $# -eq 0 ]
+then
+    echo "Current server: $HOSTNAME!"
+elif [ $1 == '-l' ] 
+then 
+    echo "Current server:$USER@$HOSTNAME!"
+elif [ $1 == '--help' ]
+then
+    echo "Usage: query_server.sh [OPTION]
+Prints the name of the current server as: \"Current Server:[servername]\"
+   -l  Prints the username with the servername
+Exit status:
+ 0  if OK,
+ 1  Problem with command line"
+else
+    echo "exit"
+    exit 1
+fi
+exit 0
